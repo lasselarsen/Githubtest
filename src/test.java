0088dc6dee18c68b6b04f0438c6f35fc62a1c145
Lasse <lasse_sl@me.com>
2018-10-30 13:37:37 +0100

    Jeg har tilføjet en print

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hej");
         System.out.println("Farvel");
+        System.out.println("Hej igen");
     }
 }
